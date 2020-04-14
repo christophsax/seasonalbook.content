00b1cd63872e526953192f8c0725f6993e2b6f7b
Christoph Sax <christoph.sax@gmail.com>
2020-04-14 10:50:38 +0200

    More example series

--- topics/daily-series/script/getting-started.R
+++ topics/daily-series/script/getting-started.R
@@ -1,4 +1,12 @@
 library(tidyverse)
 library(tsbox)
 
-x <- read_csv("data/data.csv", col_types = cols())
+snb_main <- read_csv("data/data.csv", col_types = cols())
+
+# More example series from trendecon
+trendecon <-
+  read_csv("https://raw.githubusercontent.com/trendecon/data/master/daily/trendecon_keywords.csv", col_types = cols()) %>%
+  filter(id == "orig") %>%
+  select(-id)
+
+
