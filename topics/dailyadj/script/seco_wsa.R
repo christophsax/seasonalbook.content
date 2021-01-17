11f52502b7993fce60652021391fc43bdb3b1987
Christoph Sax <christoph.sax@gmail.com>
2021-01-17 19:22:38 +0100

    update valentino

--- topics/dailyadj/script/seco_wsa.R
+++ topics/dailyadj/script/seco_wsa.R
@@ -8,44 +8,43 @@ library(tempdisagg)
 library(tsbox)
 library(tidyverse)
 
-dta <- read_csv(here::here("data/seco.csv")) %>%
-  select(id, time, value)
-
-
-sbb_nettotonnenkm <-
-  dta %>%
-  ts_pick("sbb_nettotonnenkm") %>%
-  select(-id) %>%
-  ts_regular()
-
-
-wwa_ntkm <-
-  dta %>%
-  ts_pick("wwa_ntkm") %>%
-  select(-id) %>%
-  ts_regular()
+load(here::here("data/sbb.Rdata"))
 
 
 
 # fill in NAs
-ts_na_interpolation <- ts_(function(x, ...) imputeTS::na_interpolation(x, ...))
-wwa_ntkm_interpol <- wwa_ntkm %>%
-  ts_na_interpolation()
-wwa_ntkm$value[is.na(wwa_ntkm$value)] <- wwa_ntkm_interpol$value[is.na(wwa_ntkm$value)]
+interpol_weeks <- function(x) {
+  ts_na_interpolation <- ts_(function(x, ...) imputeTS::na_interpolation(x, ...))
+  x_interpol <- x %>%
+    ts_na_interpolation()
+  x$value[is.na(x$value)] <- x_interpol$value[is.na(x$value)]
+  x
+}
+
+raw <-
+  sbb_sax %>%
+  ts_pick("raw") %>%
+  select(-id) %>%
+  ts_regular() %>%
+  interpol_weeks()
+
+wsa <-
+  sbb_sax %>%
+  ts_pick("wsa") %>%
+  select(-id) %>%
+  ts_regular() %>%
+  interpol_weeks()
 
 
-
-# filter(wwa_ntkm, is.na(value))
-
+# filter(wsa, is.na(value))
+# filter(raw, is.na(value))
 
 # library(tempdisagg)
 # # 1. disaggregate to daily
 
 # # fast means faster than the other methods, but week to day is still slow.
-# m_daily <- td(sbb ~ 1, to = "day", method = "fast")
-# sbb_nettotonnenkm <- predict(m_daily)
-
-
+m_daily <- td(raw ~ 1, to = "day", method = "fast")
+raw_daily <- predict(m_daily)
 
 
 # Temporary: tsbox should be able to do `ts_frequency(x, "week")
@@ -72,19 +71,20 @@ ts_frequency_week <- function(x, fun = function(e) mean(e, na.rm = TRUE)) {
 }
 
 # series are the same (week starts one day off)
-sbb_nettotonnenkm_nsa <-
-  ts_frequency_week(ts_lag(sbb_nettotonnenkm, -1), sum) %>%
+raw_alt <-
+  ts_frequency_week(ts_lag(raw_daily, -1), sum) %>%
   mutate(time = tsbox:::time_shift(time, "1 day"))
 
+# raw %ts-% raw_alt
 
 # 2. Seasonally adjust artificial daily data
-sbb_nettotonnenkm_sa_raw <- seas_daily(sbb_nettotonnenkm)
+sa_daily <- seas_daily(raw_daily)
 
-plot_components(sbb_nettotonnenkm_sa_raw)
+plot_components(sa_daily)
 
 # 3. Aggregate back to weekly
-sbb_nettotonnenkm_sa <-
-  sbb_nettotonnenkm_sa_raw %>%
+sa <-
+  sa_daily %>%
   ts_pick("adj") %>%
   select(-id) %>%
   # ts_pcy() %>%
@@ -92,23 +92,14 @@ sbb_nettotonnenkm_sa <-
   # ts_lag(-1) %>%
   ts_frequency_week(sum)
 
-ts_plot(sbb_nettotonnenkm_nsa, sbb_nettotonnenkm_sa)
+ts_plot(raw, sa)
+
+
+ts_plot(ts_span(ts_c(sa, wsa), 2005))
 
 
 
-ts_plot(wwa_ntkm)
-
-ts_plot(sbb_nettotonnenkm_sa)
-
-ts_plot(sbb_nettotonnenkm_nsa)
-
-
-ts_scale(ts_c(
-ts_frequency(wwa_ntkm, "year"),
-ts_frequency(sbb_nettotonnenkm_nsa, "year"),
-ts_frequency(sbb_nettotonnenkm_sa, "year")
-)) %>%
-ts_plot()
+# sa$value <- zoo::rollmean(sa$value, 3, align="right", fill=NA)
 
 
 
