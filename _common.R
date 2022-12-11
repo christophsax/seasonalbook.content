f8ba186cb42ce21397f047a2d6f84e7a9eb22929
Christoph Sax <christoph@cynkra.com>
2022-12-11 08:16:46 +0100

    course date

--- _common.R
+++ _common.R
@@ -22,14 +22,28 @@ options(
 
 ggplot2::theme_set(ggplot2::theme_gray(12))
 
-status <- function(type) {
+# status("drafting", Sys.Date())
+status <- function(type, date = as.Date(NA)) {
   status <- switch(type,
-    polishing = "should be readable but needs more polishing",
+    polishing = "should be readable but needs polishing",
     restructuring = "is undergoing heavy restructuring and may be confusing or incomplete",
     drafting = "is currently a dumping ground for ideas, and we don't recommend reading it",
     complete = "is largely complete and just needs final proof reading",
     stop("Invalid `type`", call. = FALSE)
   )
+
+  if (!is.na(date)) {
+    date <- as.Date(date)
+    course_info <- paste0(
+      "\n**It is part of the course materials intended for ",
+      format(date, "%b %d, %Y"),
+      "**.\n"
+    )
+  } else {
+    course_info <- ""
+  }
+
+  # append_df(data.frame(type = type, date = date))
 
   class <- switch(type,
     polishing = "note",
@@ -44,7 +58,27 @@ status <- function(type) {
     "::: callout-", class, " \n",
     "You are reading an early draft of *Seasonal Adjustment in R*. ",
     "This chapter ", status, ".\n",
+    course_info,
     ":::\n",
     "::::\n"
   ))
 }
+
+# # Unclear what's a good way to share date between qmd. This writes a file
+# # to ~ for now.
+# # append_df(refresh = TRUE)          # delete
+# # append_df()                        # show
+# # append_df(data.frame(a = "dfsf"))  # append
+# append_df <- function(row = NULL,
+#                       name = "status",
+#                       path = fs::path("~", paste0(name, ".rds")),
+#                       refresh = FALSE) {
+#   r <- if (file.exists(path)) readr::read_rds(path) else NULL
+#   ans <- rbind(r, row)
+#   if (is.null(row)) {
+#     if (refresh && file.exists(path)) fs::file_delete(path)
+#   } else {
+#     readr::write_rds(ans, path)
+#   }
+#   ans
+# }
